97b64d32852711a8b7374e62d97ac5a0f5ace2e4
Erick <erickhenriquepiacente@gmail.com>
2026-04-02 09:34:08 -0300

    Testando .icons(tirar espaços e colocar em maiusculo

--- Cifrador.java
+++ Cifrador.java
@@ -5,9 +5,12 @@ public class Cifrador{
         Scanner leia = new Scanner(System.in);
         System.out.println("Informe a frase: ");
         String frase = leia.nextLine();
+        frase = frase.replace(" ", "");
+        frase = frase.toUpperCase();
+        System.out.println(frase);
         char[] letra =  {'A', 'B', 'C', 'D','E', 'F', 'G', 'H','I', 'J', 'K', 'L','M', 'N', 'O', 'P','Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'};
         for(int i = 0; i <  letra.length; i++){
-            System.out.print(" "+letra[i]+" ");
+
 
         }
         
