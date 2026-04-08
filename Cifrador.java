5205d365b5e0624c600c2b2315d76eb2f58377c5
ErickPiacente <erickhenriquepiacente@gmail.com>
2026-04-08 14:47:45 -0300

    Fazendo e testando o cifrador

--- Cifrador.java
+++ Cifrador.java
@@ -8,15 +8,19 @@ public class Cifrador{
         frase = frase.replace(" ", "");
         frase = frase.toUpperCase();
         System.out.println(frase);
-        char[] letra =  {'A', 'B', 'C', 'D','E', 'F', 'G', 'H','I', 'J', 'K', 'L','M', 'N', 'O', 'P','Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'};
-        for(int i = 0; i <  letra.length; i++){
-
-
+        int tamanho = frase.length();
+        int indice = 0;
+        int posição = 0;
+        while (indice < frase.length()) {
+        System.out.print((frase.charAt(posição))); //Arrumar o salto que está dando algum erro de calculo
+        
+        posição = (posição + 5) % tamanho;
+        indice++;
         }
         
 
-        int indice = 0;
-        indice = (indice + 5) % 47;
+        
+        
         
 
     }
